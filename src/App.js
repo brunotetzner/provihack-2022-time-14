67ba14cedb3e6b951b7f52133e385879c2f2d93d
LayaneB <layanejuvito@gmail.com>
2022-05-01 17:13:19 -0300

    Adicionando a section na landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Router from "./routes";
+import Router from './routes'
 import { GlobalStyle } from "./themes/globalStyle";
 function App() {
   return (
--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,12 +1,14 @@
 import AboutUsSection from "../../components/aboutUsSection"
 import Footer from "../../components/footer"
 import Join from "../../components/join"
+import News from "../../components/news"
 import PlanBSection from "../../components/planBSection"
 
 const LandingPage = ()=>{
     return(
     <div>
         <PlanBSection />
+        <News />
         <AboutUsSection />
         <Join />
         <Footer />
