cb838d5980f1c77f9fd518afd02fe1ddd306d398
Ádria Tavares <71045022+adriatls@users.noreply.github.com>
2022-05-01 20:17:43 -0300

    Adria tips page (#24)
    * Estilização da Tips Page
    
    * Onclick do botão SeeHow na LandingPage

--- src/components/planBSection/index.jsx
+++ src/components/planBSection/index.jsx
@@ -1,15 +1,19 @@
+import { useNavigate } from "react-router-dom"
 import HeaderHome from "../headerHome"
 import { Content, SectionPlanB, SeeHowButton } from "./style"
 
 
 const PlanBSection = () => {
+    const navigate = useNavigate();
     return (
         <SectionPlanB>
             <HeaderHome />
             <Content>
                 <h2>Não existe um planeta B</h2>
                 <h6>Todos os cenários analisados pelo IPCC apontam para uma mesma conclusão: é  preciso cortar emissões e cortá-las o mais rápido possível.</h6>
-                <SeeHowButton>Saiba como</SeeHowButton>
+                <SeeHowButton
+                    onClick={() => navigate("/tips")}
+                >Saiba como</SeeHowButton>
             </Content>
         </SectionPlanB>
     )
